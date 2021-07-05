a51229ee5e9c16f298b182bd88047d4d5013c0ec
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-07-05 13:53:21 +0100

    Linear agent added functionality
    Added functionality to the linear game to move towards a Gym env. I may
    end up moving all of this code into the gym_mdptetris repository, but
    it's here for now.
    
    Added reset and step functions. Added some comments and type hints.
    Added game completion to return value from steps. Used for preparation
    of DQN heuristic game.

--- mdptetris_experiments/agents/linear_agent.py
+++ mdptetris_experiments/agents/linear_agent.py
@@ -1,28 +1,34 @@
 import os
 import random
 import time
+import torch
+from typing import Tuple
 import numpy as np
 import gym_mdptetris.envs
 from gym_mdptetris.envs import board, piece, feature_functions
 
 class LinearGame():
-    """
-    Linear game of Tetris for reinforcement learning.
+    def __init__(self, weights: np.ndarray = np.array([-1, 1, -1, -1, -4, -1]),
+        board_height: int = 20, 
+        board_width: int = 10,  
+        piece_set: str = 'pieces4.dat', 
+        seed: int = 12345):
+        """
+        Linear game of Tetris for reinforcement learning.
 
-    By default the feature set and weights are set to those of Pierre Dellacherie.
-    This hand tuning is excellent at the game, and runs can last for an extended
-    period of time. 
+        By default the feature set and weights are set to those of Pierre Dellacherie.
+        This hand tuning is excellent at the game, and runs can last for an extended
+        period of time. 
 
-    The state-value of a board is the sum of the product of the state's heuristic
-    representation and the weights given to each feature.
+        The state-value of a board is the sum of the product of the state's heuristic
+        representation and the weights given to each feature.
 
-    :param weights: A numpy array of weights ascribed to the feature set. 
-    :param board_height: The height of the tetris board. 
-    :param board_width: The width of the tetris board.
-    :param piece_set: Relative path to the piece set in the data directory.
-    :param seed: Seed of the random function. 
-    """
-    def __init__(self, weights=np.array([-1, 1, -1, -1, -4, -1]), board_height=20, board_width=10,  piece_set='pieces4.dat', seed=12345):
+        :param weights: A numpy array of weights ascribed to the feature set. 
+        :param board_height: The height of the tetris board. 
+        :param board_width: The width of the tetris board.
+        :param piece_set: Relative path to the piece set in the data directory.
+        :param seed: Seed of the random function. 
+        """
         self.weights = weights
         self.board_height = board_height
         self.board_width = board_width
@@ -35,10 +41,11 @@ class LinearGame():
                 self.max_piece_height = max(self.max_piece_height, o.height)
         random.seed(seed)
         self.new_piece()
+        self.lines_cleared = 0
         self.board = board.Board(max_piece_height=self.max_piece_height,
             width=board_width, height=board_height)
 
-    def get_dellacherie_features(self) -> np.ndarray:
+    def get_dellacherie_features(self) -> torch.FloatTensor:
         """
         Get the heuristic representation of a board state as the set of
         6 Dellacherie features.
@@ -46,24 +53,30 @@ class LinearGame():
         :return: Numpy array of the Dellacherie features of the current
             board state.
         """
-        res = np.empty((6), dtype='double')
-        for i, f in enumerate(feature_functions.get_dellacherie_funcs()):
-            res[i] = f(self.board)
-        return res 
+        res = []
+        for f in feature_functions.get_dellacherie_funcs():
+            res.append(f(self.board))
+        return torch.FloatTensor(res)
     
-    def new_piece(self):
+    def new_piece(self) -> None:
         """
         Select a new piece. 
         """
         self.current_piece = random.choice(range(self.nb_pieces))
     
-    def seed(self, seed_value: int):
+    def seed(self, seed_value: int) -> None:
         """
         Provide a new seed for the environment.
         """
         random.seed(seed_value)
 
-    def get_next_states(self):
+    def reset(self) -> torch.FloatTensor:
+        self.board.reset()
+        self.new_piece()
+        self.lines_cleared = 0
+        return self.get_dellacherie_features()
+
+    def get_next_states(self) -> dict:
         """
         Returns a dictionary of subsequent states that are reachable with the
         current board state and piece. Used in state-value implementations.
@@ -78,16 +91,33 @@ class LinearGame():
                 states[i,j] = self.get_dellacherie_features()
                 self.board.cancel_last_move()
         return states
-
-    def board_step(self):
+    
+    def step(self, action: Tuple[int, int]) -> Tuple[int, bool]:
         """
-        Make one action step. Given a board state generate all possible 
-        successive states. Use the dellacherie features and the game feature 
-        weights to calculate the state-value of the successive states. 
-        Find the maximum state value of a successive state and commit the
-        required action to reach it. 
+        Make one action step given the action. 
 
-        :return: Returns the number of lines cleared by the step
+        :param action: tuple of piece orientation and column for placement
+        :return:
+            reward: The number of lines cleared by the step
+            done: Boolean indicating if the game is over
+        """
+        reward = self.board.drop_piece(self.pieces[self.current_piece].orientations[action[0]], action[1])
+        done = self.board.wall_height > self.board_height
+        self.lines_cleared += reward
+        return reward, done
+    
+
+    def board_step(self) -> Tuple[int, bool]:
+        """
+        Make one action step using the predetermined weights. Given a board
+        state generate all possible successive states. Use the dellacherie
+        features and the game feature weights to calculate the state-value of
+        the successive states.  Find the maximum state value of a successive
+        state and commit the required action to reach it. 
+        
+        :return: 
+            reward: The number of lines cleared by the step
+            done: Boolean indicating if the game is over
         """
         actions = np.full((4, self.board_width), -np.inf, dtype='double')
         for i in range(self.pieces[self.current_piece].nb_orientations):
@@ -97,12 +127,17 @@ class LinearGame():
                 actions[i,j] = (self.get_dellacherie_features() * self.weights).sum()
                 self.board.cancel_last_move()
         a = np.unravel_index(np.argmax(actions), actions.shape)
-        return self.board.drop_piece(self.pieces[self.current_piece].orientations[a[0]], a[1])
+        reward = self.board.drop_piece(self.pieces[self.current_piece].orientations[a[0]], a[1])
+        done = self.board.wall_height > self.board_height
+        return reward, done
 
-    def play_game(self, render=False):
+    def play_game(self, render=False) -> None:
         cleared = 0
-        while self.board.wall_height < self.board_height and cleared < 1e4:
-            cleared += self.board_step()
+
+        done = False
+        while not done and cleared < 1e4:
+            reward, done = self.board_step()
+            cleared += reward
             self.new_piece()
             if render:
                 print(self.board)
