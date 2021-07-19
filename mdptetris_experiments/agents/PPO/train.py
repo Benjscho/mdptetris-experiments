6004de750bc01b7b80a41319ea65bcc5cd7aea53
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-07-19 13:08:20 +0100

    Scrap previous for PPO
    Scrapping previous for PPO, going to build a class based agent. Need to
    decide whether to make it model based or model free and how to construct
    either of those.

--- mdptetris_experiments/agents/PPO/train.py
+++ mdptetris_experiments/agents/PPO/train.py
@@ -48,160 +48,59 @@ def get_args() -> argparse.Namespace:
     return args
 
 state_rep = {
-    "heuristic": [NNHeuristic, LinearGame],
+    "heuristic": [NNHeuristic, MultiLinearGame],
     "1D": [NN1D, LinearGameStandard]
 }
 
-def train(args: argparse.Namespace):
-    """
-    Method that initialises a network with a set of arguments and trains
-    it on a set of given Tetris environment.
-
-    :param args: A namespace containing the hyperparameters and arguments
-        for training the model.
-    """
-    # Set up multiple envs
-
-    env = state_rep[args.state_rep][1](board_height=args.board_height,
-                     board_width=args.board_width)
+class PPO():
+    def __init__(self, args: argparse.Namespace):
+        self.args = args
 
 
-    # Generic setup of device, runid, save location, tensorboard etc.
-    device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
-    runid = time.strftime('%Y%m%dT%H%M%SZ')
-    save_dir = f"{args.save_dir}-{runid}"
-    # Writer for TensorBoard
-    writer = SummaryWriter(args.log_dir, comment=f"{args.comment}-{runid}")
-    if not os.path.isdir(save_dir):
-        os.makedirs(save_dir)
-    with open(f"{save_dir}/args.txt", 'w') as f:
-        f.write(str(args))
+    def train(self, total_timesteps):
+        """
+        Train the agent for a number of timesteps. 
+        """
+        pass
 
+    def rollout(self):
+        """
+        Conduct a rollout 
+        """
+        state_b = []
+        action_b = []
+        log_probs_b = []
+        rewards_b = []
+        rewards_tg_b = []
+        ep_len_b = []
 
-    # Set up model, network, optimizer, and memory buffer
-    if args.load_file != None:
-        model = torch.load(args.load_file)
-    else: 
-        model = state_rep[args.state_rep][0]().to(device)
-    target = state_rep[args.state_rep][0]().to(device)
-    target.load_state_dict(model.state_dict())
-    target.eval()
+        # Track rewards per episode
+        ep_rewards = []
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.alpha)
-    replay_buffer = deque(maxlen=args.replay_buffer_length)
-    loss_criterion = nn.MSELoss()
-    epochs = np.array([], dtype=np.int32)
-    timesteps = np.array([], dtype=np.int32)
+        timesteps = 0
+        while timesteps < batch_timesteps:
+            ep_rewards = []
 
-    # Seed randomness
-    if args.seed == None:
-        seed = int(time.time())
-        random.seed(seed)
-        env.seed(seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed(seed)
-        else:
-            torch.manual_seed(seed)
-    else:
-        random.seed(args.seed)
-        env.seed(args.seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed(args.seed)
-        else:
-            torch.manual_seed(args.seed)
+            obs = env.reset()
+            done = False
 
-    state = env.reset().to(device)
+            
 
-    epoch = 0
-    timestep = 0
-    while epoch < args.epochs:
-        action_states = env.get_next_states()
-        epsilon = args.final_epsilon + (max(args.epsilon_decay_period - epoch, 0) * (
-            args.init_epsilon - args.final_epsilon) / args.epsilon_decay_period)
-        new_actions, new_states = zip(*action_states.items())
-        new_states = torch.stack(new_states).to(device)
+    def get_action(self, state):
+        """
+        Query the actor network to get the action.
 
-        # Predict values of next states
-        model.eval()
-        with torch.no_grad():
-            predictions = model(new_states)[:, 0]
-        model.train()
+        :param state: observation at the current timestep
 
-        # Select next state with epsilon greedy strategy
-        if random.random() <= epsilon:
-            idx = random.randint(0, len(new_actions) - 1)
-        else:
-            idx = torch.argmax(predictions).item()
+        return:
+            action: The action to take
+            log_prob: The log probability of the selected action
+        """
 
-        new_state = new_states[idx, :].to(device)
-        action = new_actions[idx]
-        reward, done = env.step(action)
-        timestep += 1
+        res = actor_network(state)
 
-        # Append step to buffer and save reward
-        # If I want to save more infrequent reward I can use the buffer to batch
-        # results across timesteps.
-        replay_buffer.append([state, reward, new_state, done])
-        timesteps = np.append(timesteps, reward)
-
-        # Skip epoch increment if episode is not done. If done, record episode
-        # score and reset env. 
-        if done:
-            episode_score = env.lines_cleared
-            state = env.reset().to(device)
-        else:
-            state = new_state
-            continue
-
-        # Skip training until memory buffer is 1/10th full
-        if len(replay_buffer) < args.replay_buffer_length / 10:
-            continue
-
-        epoch += 1
-
-        batch = random.sample(replay_buffer, min(
-            len(replay_buffer), args.batch_size))
-        state_b, reward_b, new_state_b, done_b = zip(*batch)
-        state_b = torch.stack(state_b).to(device)
-        reward_b = torch.from_numpy(
-            np.array(reward_b, dtype=np.float32)[:, None]).to(device)
-        new_state_b = torch.stack(new_state_b).to(device)
-
-        # Use model to judge state values, train prediction against target network
-        q_vals = model(state_b).to(device)
-        with torch.no_grad():
-            next_predictions = target(new_state_b)
-
-        y_b = torch.cat(
-            tuple(reward if done else reward + args.gamma * prediction for reward, done, prediction in
-                  zip(reward_b, done_b, next_predictions))
-        )[:, None].to(device)
-
-        # Calculate loss and train network
-        optimizer.zero_grad()
-        loss = loss_criterion(q_vals, y_b)
-        loss.backward()
-        optimizer.step()
-
-        # Update the target network
-        if epoch % args.target_network_update == 0:
-            target.load_state_dict(model.state_dict())
-
-        epochs = np.append(epochs, episode_score)
-        print(f"Epoch: {epoch}, score: {episode_score}")
-        writer.add_scalar(f'Train-{runid}/Lines cleared per epoch',
-                          episode_score, epoch - 1)
-        writer.add_scalar(f'Train-{runid}/Lines cleared over last 100 timesteps',
-                          sum(timesteps[-100:]), timestep - 1)
-
-        # On interval, save model and current results to csv 
-        if epoch % args.saving_interval == 0:
-            torch.save(model, f"{save_dir}/model")
-            epochs.tofile(f"{save_dir}/epochs.csv", sep=',')
-            timesteps.tofile(f"{save_dir}/timesteps.csv", sep=',')
-
-
-if __name__ == '__main__':
-    # Train the model
-    args = get_args()
-    train(args)
+    def save(self):
+        """
+        Save current agent state and associated run log details. 
+        """
+        pass
