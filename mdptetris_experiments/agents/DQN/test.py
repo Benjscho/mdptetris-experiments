a83a5939f3ba66528f3fddcbad87d57c3999fac6
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-07-20 14:49:32 +0100

    Flesh out test function
    Flesh out test function. Need to add performance logging, and whether to
    record runs. Recording runs would also need the render from the
    environment to fulfil the Gym requirements.

--- mdptetris_experiments/agents/DQN/test.py
+++ mdptetris_experiments/agents/DQN/test.py
@@ -16,6 +16,7 @@ def get_args() -> argparse.Namespace:
     parser.add_argument("--load_file", type=str, default=None,
                         help="Path to trained model")
     parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--total_timesteps", type=int, default=10000)
     parser.add_argument("--save_dir", type=str,
                         default=f"runs/run-info")
 
@@ -37,6 +38,8 @@ def test(args: argparse.Namespace):
     runid = time.strftime('%Y%m%dT%H%M%SZ')
     save_dir = f"{args.save_dir}-{runid}"
     model = torch.load(args.load_file)
+    # Put model in eval mode (no training during test)
+    model.eval()
 
     if args.seed == None:
         seed = int(time.time())
@@ -54,4 +57,32 @@ def test(args: argparse.Namespace):
         else:
             torch.manual_seed(args.seed)
 
-    
+    state = env.reset().to(device)
+
+    episode = 0 
+    timestep = 0 
+    ep_score = 0
+
+    while timestep < args.total_timesteps:
+        action_states = env.get_next_states()
+        new_actions, new_states = zip(*action_states.items())
+        new_states = torch.stack(new_states).to(device)
+
+        with torch.no_grad():
+            predictions = model(new_states)[:, 0]
+
+        idx = torch.argmax(predictions).item()
+
+        new_state = new_states[idx, :].to(device)
+        action = new_actions[idx]
+        reward, done = env.step(action)
+        ep_score += reward
+        timestep += 1
+
+        if done: 
+            tot_ep_score = ep_score
+            ep_score = 0 
+            state = env.reset().to(device)
+        else:
+            state = new_state
+            continue
