0e6e209fd6f70315e082a95107bfff9319d4e4f4
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-08-12 09:53:40 +0100

    Parse args from text for analysis
    Add argument parsing from saved args for graphs and analysis

--- mdptetris_experiments/experiments/analysis/create_graphs.py
+++ mdptetris_experiments/experiments/analysis/create_graphs.py
@@ -1,21 +1,30 @@
-import argparse
+import sys
 from matplotlib import pyplot as plt
-
-def get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--runs_dir", type=str, default=None)
-
-    args = parser.parse_args()
-    return args
-
+from argparse import ArgumentParser, Namespace
+from run_arg_parser import get_parser
 
 # graph reward against timesteps
-def reward_over_time(reward):
+def reward_over_time(reward, ):
     timesteps = [i for i in range(len(reward))]
     plt.plot(timesteps, reward)
     plt.show()
 
-# graph epoch against reward
+
+def main(run_dirs): 
+    parser = get_parser()
+    for dir in run_dirs: 
+        runinfo = dir 
+        timesteps = runinfo + "timesteps.csv"
+        epochs = runinfo + "epochs.csv"
+        with open(dir + "/args.txt") as f:
+            ns = f.read()
+            args, unknown = parser.parse_known_args(namespace=eval(ns))
+        print(args)
+        print(unknown)
+    pass
+
 
 if __name__=='__main__':
-    args = get_args()
+    run_dirs = sys.argv[1:]
+    print(run_dirs)
+    main(run_dirs)
