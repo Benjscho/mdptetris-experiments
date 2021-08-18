4ea20fc82b3427b3ee89f8fe179fede30614e5da
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-08-18 13:38:36 +0100

    First pass of results graphs
    Completed the first pass on the results graphs. Implemented an automated
    process for redrawing them straight to the TeX directory for my
    dissertation.

--- mdptetris_experiments/experiments/analysis/create_graphs.py
+++ mdptetris_experiments/experiments/analysis/create_graphs.py
@@ -1,12 +1,13 @@
+import ast
 import csv
 import sys
 from argparse import ArgumentParser, Namespace
 from typing import List
 
+import matplotlib
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-import matplotlib
 
 from run_arg_parser import get_parser
 
@@ -15,9 +16,12 @@ matplotlib.rcParams['font.family'] = 'STIXGeneral'
 matplotlib.pyplot.title(r'ABC123 vs $\mathrm{ABC123}^{123}$')
 
 
+SAVE_DIR = "/Users/crow/Desktop/bath-msc-main/cm50170-Diss/Dissertation/resources/"
 ROOT_RUN_DIR = "/Users/crow/Desktop/bath-msc-main/cm50170-Diss/experiment-results/main-runs/run-info-"
-MBDQN_RUNS = [ROOT_RUN_DIR + i for i in ["20210809T161605Z", "20210811T071836Z"]]
-MBDQN_1PIECE_RUNS = [ROOT_RUN_DIR + i for i in ["20210713T101844Z", "20210803T070646Z"]]
+MBDQN_RUNS = [ROOT_RUN_DIR +
+              i for i in ["20210809T161605Z", "20210811T071836Z"]]
+MBDQN_1PIECE_RUNS = [ROOT_RUN_DIR +
+                     i for i in ["20210713T101844Z", "20210803T070646Z"]]
 PPO_RUNS = [ROOT_RUN_DIR + i for i in ["20210730T093417Z", "20210802T205340Z"]]
 
 
@@ -46,7 +50,7 @@ def read_result_csv(file_path):
     return t[0]
 
 
-def analyse_MBDQN(run_dirs: List[str], title: str):
+def analyse_MBDQN(run_dirs: List[str], title: str, save_file: str):
     run_args = get_run_args(run_dirs)
     run_timesteps = {}
     run_epochs = {}
@@ -55,17 +59,19 @@ def analyse_MBDQN(run_dirs: List[str], title: str):
         run_timesteps[dir] = read_result_csv(dir + "/timesteps.csv")
         run_epochs[dir] = read_result_csv(dir + "/epochs.csv")
 
+    plt.figure(save_file)
     plt.title(title)
     grouping = 20
     for dir in run_dirs:
         df = pd.DataFrame(run_epochs[dir])
         df = df.astype(float)
         df = smooth_data(df, 0.8, grouping)
-        plt.plot([i*grouping for i in range(len(df))], df, label=run_args[dir].state_rep.capitalize())
-    plt.legend()
+        plt.plot([i*grouping for i in range(len(df))], df,
+                 label=run_args[dir].state_rep.capitalize())
+    plt.legend(title="State representation")
     plt.xlabel("Training epoch")
-    plt.ylabel(f"Average reward per {grouping} epochs")
-    plt.show()
+    plt.ylabel(f"Average reward per episode, grouped over {grouping} epochs")
+    plt.savefig(SAVE_DIR + save_file + ".png")
 
 
 def smooth_data(df: pd.DataFrame, alpha: float = 0.9, grouping: int = 100):
@@ -77,34 +83,38 @@ def smooth_data(df: pd.DataFrame, alpha: float = 0.9, grouping: int = 100):
     return tdf.ewm(alpha=(1 - alpha)).mean()
 
 
-def analyse_PPO(run_dirs: List[str], title: str):
-    run_args = get_run_args(run_dirs)
+def analyse_PPO(run_dirs: List[str], title: str, save_file: str):
+    run_args = {}
+    for dir in run_dirs:
+        with open(dir + "/args.txt") as f:
+            data = f.read()
+            run_args[dir] = ast.literal_eval(data)
+
     avg_rewards = {}
     timesteps = {}
 
     for dir in run_dirs:
-        timesteps[dir] = read_result_csv(dir + "/epoch_timesteps.csv")
-        avg_rewards[dir] = read_result_csv(dir + "/avg_ep_rewards.csv")
+        timesteps[dir] = pd.read_csv(dir + "/rewards.csv")
 
+    plt.figure(save_file)
     plt.title(title)
-    grouping = 20
+    grouping = 1
     for dir in run_dirs:
-        df = pd.DataFrame([avg_rewards[dir], timesteps[dir]])
-        df = df.astype(float)
-        print(df)
+        df = timesteps[dir]
         df = smooth_data(df, 0.8, grouping)
-        print(df)
-        plt.plot([i*grouping for i in range(len(df))], df, label=run_args[dir].state_rep.capitalize())
-    plt.legend()
+        plt.plot(df['Step'], df['Value'], label=run_args[dir]['board_height'])
+    plt.legend(title="Board height")
     plt.xlabel("Timesteps")
-    plt.ylabel(f"Average reward per {grouping} epochs")
-    plt.show()
+    plt.ylabel(f"Average reward per timestep")
+    plt.savefig(SAVE_DIR + save_file + ".png")
 
 
 def main():
-    analyse_PPO(PPO_RUNS, "PPO Learning rate")
-    analyse_MBDQN(MBDQN_1PIECE_RUNS, "(a) MBDQN learning for one piece per episode")
-    analyse_MBDQN(MBDQN_RUNS, "(b) MBDQN learning on standard Tetris")
+    analyse_PPO(PPO_RUNS, "PPO Learning rate", "ppo")
+    analyse_MBDQN(MBDQN_1PIECE_RUNS,
+                  "(a) MBDQN learning rate for single piece episodes", "mbdqn-1piece")
+    analyse_MBDQN(
+        MBDQN_RUNS, "(b) MBDQN learning rate on standard Tetris", "mbdqn")
 
 
 if __name__ == '__main__':
