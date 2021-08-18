206dd7c988f7bb27d0a49b2bb23b0b215d4f7b48
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-08-18 11:32:11 +0100

    Add MBDQN graph analysis
    Developed the main graph analysis and style for the graphs, now
    translating to PPO runs too. Need to confirm style for grid lines and
    complete PPO graph making. This can then be an automated process that
    saves run graphs to my dissertation folder for use in the writeup.

--- mdptetris_experiments/experiments/analysis/create_graphs.py
+++ mdptetris_experiments/experiments/analysis/create_graphs.py
@@ -6,8 +6,14 @@ from typing import List
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import matplotlib
 
 from run_arg_parser import get_parser
+
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.pyplot.title(r'ABC123 vs $\mathrm{ABC123}^{123}$')
+
 
 ROOT_RUN_DIR = "/Users/crow/Desktop/bath-msc-main/cm50170-Diss/experiment-results/main-runs/run-info-"
 MBDQN_RUNS = [ROOT_RUN_DIR + i for i in ["20210809T161605Z", "20210811T071836Z"]]
@@ -40,41 +46,65 @@ def read_result_csv(file_path):
     return t[0]
 
 
-def analyse_MBDQN(run_dirs: List[str]):
+def analyse_MBDQN(run_dirs: List[str], title: str):
     run_args = get_run_args(run_dirs)
     run_timesteps = {}
     run_epochs = {}
 
-    len_t = 0
-    len_e = 0
-
     for dir in run_dirs:
         run_timesteps[dir] = read_result_csv(dir + "/timesteps.csv")
         run_epochs[dir] = read_result_csv(dir + "/epochs.csv")
-        len_t = max(len_t, len(run_timesteps[dir]))
-        len_e = max(len_e, len(run_epochs[dir]))
 
+    plt.title(title)
+    grouping = 20
     for dir in run_dirs:
         df = pd.DataFrame(run_epochs[dir])
         df = df.astype(float)
-        # Grouping results: https://stackoverflow.com/a/36810658/14354978
-        df = smooth_data(df)
-        plt.plot(df)
+        df = smooth_data(df, 0.8, grouping)
+        plt.plot([i*grouping for i in range(len(df))], df, label=run_args[dir].state_rep.capitalize())
+    plt.legend()
+    plt.xlabel("Training epoch")
+    plt.ylabel(f"Average reward per {grouping} epochs")
     plt.show()
 
 
 def smooth_data(df: pd.DataFrame, alpha: float = 0.9, grouping: int = 100):
-    tdf = df.groupby(np.arange(len(df))//100).mean()
-    return tdf.ewm(alpha=(1 - 0.9)).mean()
+    """
+    Method to group results of a dataframe and smooth with an alpha factor. 
+    Attribution: https://stackoverflow.com/a/36810658/14354978
+    """
+    tdf = df.groupby(np.arange(len(df))//grouping).mean()
+    return tdf.ewm(alpha=(1 - alpha)).mean()
 
 
-def analyse_PPO(run_dirs):
-    pass
+def analyse_PPO(run_dirs: List[str], title: str):
+    run_args = get_run_args(run_dirs)
+    avg_rewards = {}
+    timesteps = {}
+
+    for dir in run_dirs:
+        timesteps[dir] = read_result_csv(dir + "/epoch_timesteps.csv")
+        avg_rewards[dir] = read_result_csv(dir + "/avg_ep_rewards.csv")
+
+    plt.title(title)
+    grouping = 20
+    for dir in run_dirs:
+        df = pd.DataFrame([avg_rewards[dir], timesteps[dir]])
+        df = df.astype(float)
+        print(df)
+        df = smooth_data(df, 0.8, grouping)
+        print(df)
+        plt.plot([i*grouping for i in range(len(df))], df, label=run_args[dir].state_rep.capitalize())
+    plt.legend()
+    plt.xlabel("Timesteps")
+    plt.ylabel(f"Average reward per {grouping} epochs")
+    plt.show()
 
 
 def main():
-
-    analyse_MBDQN(MBDQN_RUNS)
+    analyse_PPO(PPO_RUNS, "PPO Learning rate")
+    analyse_MBDQN(MBDQN_1PIECE_RUNS, "(a) MBDQN learning for one piece per episode")
+    analyse_MBDQN(MBDQN_RUNS, "(b) MBDQN learning on standard Tetris")
 
 
 if __name__ == '__main__':
